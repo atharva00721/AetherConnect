01fb3355b3676cf06f5b7d0e4e5449518332445c
Atharva Raj Thakur <69038796+atharva00721@users.noreply.github.com>
2024-05-24 23:21:42 +0530

    UI changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 
 import MeetingTypeList from "@/components/meetingTypeList";
 import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 
 const Home = () => {
   const now = new Date();
@@ -13,15 +14,32 @@ const Home = () => {
   const date = new Intl.DateTimeFormat("en-IN", { dateStyle: "long" }).format(
     now
   );
+  let day = now.getDay();
+  const getDayOfWeek = (day: number): string => {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return daysOfWeek[day];
+  };
+
+  const dayOfWeek = getDayOfWeek(day);
   return (
     <section className="flex size-full flex-col gap-10">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base text-[#f1f1f1] font-semibold">
-            Up Coming Meeting At: 12:00 PM
+          <h2 className="glassmorphism max-w-[80px] rounded-lg py-2 text-center text-base text-[#f1f1f1] font-bold">
+            {dayOfWeek}
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">
+              {time}
+            </h1>
             <p className="text-lg font-medium text-sky-1">{date}</p>
           </div>
         </div>
--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
+import { Camera, Copy } from "lucide-react";
 import { useUser } from "@clerk/nextjs";
 import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
-
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Button, ButtonGroup } from "@nextui-org/button";
 import { useGetCallById } from "@/hooks/useGetCallById";
-import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 
 import React from "react";
@@ -22,7 +22,7 @@ const Table = ({
       <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
         {title}:
       </h1>
-      <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+      <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl md:max-w-[300px]">
         {description}
       </h1>
     </div>
@@ -52,30 +52,44 @@ const PersonalRoom = () => {
     router.push(`/meeting/${meetingId}?personal=true`);
   };
   return (
-    <section className="flex size-full flex-col gap-10">
-      <h1 className="text-3xl font-bold">Personal Room</h1>
-      <div className="flex w-full flex-col gap-8 xl:max-w-[900px]">
-        <Table title="Topic" description={`${user?.username}'s Meeting Room`} />
+    <Card
+      shadow="sm"
+      className="flex size-full flex-col gap-10 bg-dark-1 px-3 py-2 rounded-lg max-md:max-w-[400px]"
+    >
+      <CardHeader className="flex gap-3">
+        <div className="glassmorphism p-2 rounded-lg">
+          <Camera color="white" size={48} />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-3xl font-bold">Personal Room</p>
+          <p className="text-small text-default-500">{`${user?.username}'s Meeting Room`}</p>
+        </div>
+      </CardHeader>
+      <hr />
+      <CardBody className="flex w-full flex-col gap-8 xl:max-w-[900px] ">
+        {/* <Table title="Topic" description={`${user?.username}'s Meeting Room`} /> */}
         <Table title="Meeting ID" description={meetingId!} />
         <Table title="Invite Link" description={meetingLink!} />
-      </div>
-      <div className="flex gap-5">
-        <Button className="bg-blue-1" onClick={startRoom}>
-          Start Meeting
-        </Button>
-        <Button
-          className="bg-dark-3"
-          onClick={() => {
-            navigator.clipboard.writeText(meetingLink);
-            toast({
-              title: "Link Copied",
-            });
-          }}
-        >
-          Copy Invitation
-        </Button>
-      </div>
-    </section>
+      </CardBody>
+      <CardFooter className="flex gap-5">
+        <ButtonGroup className="">
+          <Button className="bg-blue-1 rounded-l-lg" onClick={startRoom}>
+            Start Meeting
+          </Button>
+          <Button
+            className="bg-dark-3 rounded-r-lg"
+            onClick={() => {
+              navigator.clipboard.writeText(meetingLink);
+              toast({
+                title: "Link Copied",
+              });
+            }}
+          >
+            <Copy />
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
   );
 };
 
