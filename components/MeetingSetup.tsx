c20ef7c11980076f2431a5ce7bd57c45c210efad
Atharva Raj Thakur <69038796+atharva00721@users.noreply.github.com>
2024-05-25 03:08:18 +0530

    Update MeetingSetup.tsx

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,28 +1,36 @@
-"use clinet";
+'use client';
+import { useEffect, useState } from 'react';
 import {
   DeviceSettings,
   VideoPreview,
   useCall,
   useCallStateHooks,
-} from "@stream-io/video-react-sdk";
-import React, { useEffect, useState } from "react";
-import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
-import Alert from "./Alert";
+} from '@stream-io/video-react-sdk';
+
+import Alert from './Alert';
+import { Button } from './ui/button';
 
 const MeetingSetup = ({
   setIsSetupComplete,
 }: {
   setIsSetupComplete: (value: boolean) => void;
 }) => {
-  const router = useRouter();
-  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+  const callStartsAt = useCallStartsAt();
+  const callEndedAt = useCallEndedAt();
+  const callTimeNotArrived =
+    callStartsAt && new Date(callStartsAt) > new Date();
+  const callHasEnded = !!callEndedAt;
+
   const call = useCall();
+
   if (!call) {
     <Alert
-      title={`Your Meeting Link is Invalid`}
-    />;
+        title={`Your Meeting Link is invalid. Please check the link and try again.`}
+      />
   }
+
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
 
   useEffect(() => {
     if (isMicCamToggled) {
@@ -34,20 +42,33 @@ const MeetingSetup = ({
     }
   }, [isMicCamToggled, call.camera, call.microphone]);
 
+  if (callTimeNotArrived)
+    return (
+      <Alert
+        title={`Your Meeting has not started yet. It is scheduled for ${callStartsAt.toLocaleString()}`}
+      />
+    );
+
+  if (callHasEnded)
+    return (
+      <Alert
+        title="The call has been ended by the host"
+        iconUrl="/icons/call-ended.svg"
+      />
+    );
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
-      <h1 className="text-center text-2xl font-extrabold">Set Up</h1>
+      <h1 className="text-center text-2xl font-bold">Setup</h1>
       <VideoPreview />
       <div className="flex h-16 items-center justify-center gap-3">
         <label className="flex items-center justify-center gap-2 font-medium">
           <input
             type="checkbox"
-            name=""
             checked={isMicCamToggled}
             onChange={(e) => setIsMicCamToggled(e.target.checked)}
-            id=""
           />
-          Join without mic and camera
+          Join with mic and camera off
         </label>
         <DeviceSettings />
       </div>
@@ -55,10 +76,11 @@ const MeetingSetup = ({
         className="rounded-md bg-green-500 px-4 py-2.5"
         onClick={() => {
           call.join();
+
           setIsSetupComplete(true);
         }}
       >
-        Join Meeting
+        Join meeting
       </Button>
     </div>
   );
